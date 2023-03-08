60befdc57569dc1000568907ac9b5094756b097c
tarrows <rem_tate@yahoo.co.jp>
2023-03-09 00:33:11 +0900

    update format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+  :root {
+    background-color: antiquewhite;
+    --measure: 60em;
+  }
+
+  :global(*) {
+    max-width: var(--measure);
+  }
+
+  :global(html, body, div, header, nav, main, footer) {
+    max-width: none;
+  }
+</style>
